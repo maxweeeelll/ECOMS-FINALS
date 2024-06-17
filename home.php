d6ac0937ea2c56bef449997d5f0573ea8eb597bb
louizel <103820997+louizel@users.noreply.github.com>
2024-06-17 05:44:23 -0700

    Add files via upload

--- home.php
+++ home.php
@@ -11,6 +11,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+     <nav class="main-menu">
+                <ul>
+                    <li><a href="catalog.php">Home</a></li>
+                </ul>
      <h1>HELLO <?php echo $_SESSION['name'];?> , WELCOME TO FITNESS OVERLOAD</h1>
      <a href="logout.php">Logout</a>
 </body>
